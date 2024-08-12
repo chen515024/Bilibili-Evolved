bd061dd2a0ee180591953c00a56d9cd3904a73ff
陈骏鑫 <chengjunxin@liankaa.com>
2024-08-13 00:03:02 +0800

    feat: 添加缓存、列表功能

--- registry/lib/components/utils/timer/Timer.vue
+++ registry/lib/components/utils/timer/Timer.vue
@@ -1,9 +1,26 @@
 <template>
-  <div @click="printPreviewTime" class="preview-time">timer test</div>
+  <div class="preview-timer-container">
+    <div class="preview-list">
+      <div class="preview-list-item" v-for="item in previewList" :key="item.aid">
+        <div class='cover-img'>
+          <img :src="item.coverUrl" />
+        </div>
+        <div class="base-info">
+          <div class="base-info-title">{{ item.title }}</div>
+          <div class="base-info-other">
+            <span class="base-info-other-up">{{ item?.up?.name }}</span>
+            <span class="base-info-other-time">{{ item.previewTime | formatTime }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
   import { videoChange } from '@/core/observer'
+  import { PreviewTimerKeyMap } from './type.d.ts'
+
   export default Vue.extend({
     data() {
       return {
@@ -12,53 +29,89 @@
         previewTime: 0,
       }
     },
-    methods: {
-      async handleInit() { 
-        const domKey = '.bpx-player-video-wrap video'
-
-        dq(domKey).addEventListener('play', (e) => {
-          this.videoPlaying = true
-          if (this.timer) {
-            return
-          }
-
-
-          this.timer = window.setInterval(() => {
-            if (this.videoPlaying) {
-              previewTime += 1
-            }
-          }, 1000)
-        })
-
-        dq(domKey).addEventListener('pause', () => {
-           this.videoPlaying = false
-        })
-
-        dq(domKey).addEventListener('ended', () => {
-          this.videoPlaying = false
-          clearInterval(this.timer)
-          this.timer = 0
-          console.log(`浏览该视频总时长为: ${this.previewTime}秒`)
-        })
-      },
-      printPreviewTime() {
-        console.log(`当前浏览时长为: ${this.previewTime}秒`)
+    computed: {
+      previewList() {
+        const storeData = JSON.parse(localStorage.getItem(PreviewTimerKeyMap.store)) || {}
+        const list = []
+        for (let key in storeData) {
+          const item = storeData[key]
+          list.push(item)
+        }
+        return list
       }
     },
-    mounted() {
-      console.log('timer mounted 123')
-      // videoChange(this.handleInit)
+    filters: {
+      formatTime(seconds: number) {
+        const h = parseInt(seconds / 60 / 60 % 24)
+        const m = parseInt(seconds / 60 % 60)
+        const s = parseInt(seconds % 60)
+        return `${h}小时${m}分钟${s}秒`
+      }
     },
-    created() {
-      console.log('timer created')
-    }
   })
 </script>
 
 <style lang="scss">
 @import 'common';
 
-.preview-time {
-  position: fixed;
+.preview-timer-container {
+  width: 300px;
+  height: 500px;
+  background: #FFF;
+  border-radius: 6px;
+
+  .preview-list {
+    padding: 20px 0;
+    overflow-y: scroll;
+    height: 100%;
+    width: 100%;
+    box-sizing: border-box;
+
+    &-item {
+      display: grid;
+      grid-template-columns: 50px 1fr;
+      column-gap: 20px;
+      width: 100%;
+      margin-bottom: 20px;
+      padding: 10px;
+      .cover-img {
+        width: 50px;
+        height: 50px;
+        border-radius: 6px;
+        img {
+          width: 50px;
+          height: 50px;
+        }
+      }
+
+      .base-info {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        &-title {
+          width: 200px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
+
+        &-other {
+          &-up {
+            text-align: left;
+          }
+
+          &-time {
+            text-align: right;
+          }
+        }
+      }
+    }
+
+    &-item:hover {
+      cursor: pointer;
+      box-shadow:2px 2px 5px #332020;
+    }
+  }
 }
 </style>
--- registry/lib/components/utils/timer/index.ts
+++ registry/lib/components/utils/timer/index.ts
@@ -1,6 +1,9 @@
 import { defineComponentMetadata } from "@/components/define"
+import { VideoInfo } from "@/components/video/video-info"
 import { videoChange } from "@/core/observer"
+import { getUserInfo } from "@/core/user-info"
 import { allVideoUrls } from "@/core/utils/urls"
+import { PreviewTimerKeyMap } from "./type.d"
 
 let timer = 0
 let videoPlaying = false
@@ -20,8 +23,36 @@ function handleVideoPlay() {
   }, 1000)
 }
 
+// 缓存当前视频播放时长，视频结束或者暂停时缓存
+async function handleInitStoreData(aid: string, previewTime: number) {
+  let storeData = {}
+  if (localStorage.getItem(PreviewTimerKeyMap.store)) {
+    storeData = JSON.parse(localStorage.getItem(PreviewTimerKeyMap.store))
+  }
+
+  if (storeData[aid]) {
+    storeData[aid].previewTime = previewTime
+  } else {
+    const { mid } = await getUserInfo()
+    const videoInfo = new VideoInfo(aid)
+    await videoInfo.fetchInfo()
+
+    storeData[aid] = {
+      mid,
+      aid,
+      coverUrl: videoInfo.coverUrl,
+      title: videoInfo.title,
+      up: videoInfo.up,
+      previewTime,
+      createDate: Date.now()
+    }
+  }
+
+  localStorage.setItem(PreviewTimerKeyMap.store, JSON.stringify(storeData))
+}
+
 const entry = () => {
-  videoChange((e) => {
+  videoChange(async ({ aid, cid }) => {
     const domKey = '.bpx-player-video-wrap video'
     
     const settings = JSON.parse(localStorage.getItem('bilibili_player_settings'))
@@ -34,6 +65,7 @@ const entry = () => {
 
     dq(domKey).addEventListener('pause', () => {
         videoPlaying = false
+        handleInitStoreData(aid, previewTime)
     })
 
     dq(domKey).addEventListener('ended', () => {
@@ -41,6 +73,7 @@ const entry = () => {
       clearInterval(timer)
       timer = 0
       console.log(`浏览该视频总时长为: ${previewTime}秒`)
+      handleInitStoreData(aid, previewTime)
     })
   })
 }
